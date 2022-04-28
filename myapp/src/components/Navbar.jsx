c4db9dc28c99f52be339972097e469cea303f235
mizzKarenbabii <mizzkarenbabii@gmail.com>
2022-04-27 23:16:21 -0400

    added fontawesome

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 // import myLogo from "myapp\src\assets\images\kr logo.png";
 // import { FaBars, FaTimes } from 'react-icons';
+import {FontAwesomeIcon, fontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 
 const Navbar = () => {
     return (
-<nav className="navbar navbar-expand-lg navbar-light bg-primary">
+<nav className="navbar navbar-expand-lg navbar-light bg-primary dark ">
   <a className="navbar-brand" href="home"><img src="{logo}" alt="logo"  ></img></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    {/* <span className="navbar-toggler-icon"></span> */}
+    <FontAwesomeIcon icon={ faBars } style= {{ color: "#ffc8dd" }} />
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
