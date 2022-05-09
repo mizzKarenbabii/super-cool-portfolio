0c8cc58c8af18c40a1e32549f035ab185915080b
mizzKarenbabii <mizzkarenbabii@gmail.com>
2022-05-08 22:48:39 -0400

    deleted extra stuff

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -20,7 +20,6 @@ const Navbar = ( {currentPage, handlePageChange}) => {
       <a
         href="#about"
         onClick={() => handlePageChange('About')}
-        // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
         className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
       >
         About
@@ -32,7 +31,6 @@ const Navbar = ( {currentPage, handlePageChange}) => {
       <a
         href="#Projects"
         onClick={() => handlePageChange('Projects')}
-        // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
         className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
       >
         Projects
@@ -52,7 +50,6 @@ const Navbar = ( {currentPage, handlePageChange}) => {
       <a
         href="#contact"
         onClick={() => handlePageChange('Contact')}
-        // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
         className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
       >
         Contact
--- myapp/src/components/Portfoliocontainer.js
+++ myapp/src/components/Portfoliocontainer.js
@@ -12,7 +12,6 @@ import background from '../assets/images/cb479679965428ec22e87e97f810f75f.jpg'
 export default function Portfoliocontainer() {
   const [currentPage, setCurrentPage] = useState('About');
 
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
    
     if (currentPage === 'About') {
@@ -31,10 +30,7 @@ export default function Portfoliocontainer() {
 
   return (
     <div className='header-wrapper' style={{backgroundImage: `url(${background})`}}>
-      {/* We are passing the currentPage from state and the function to update it */}
       <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* <Header /> */}
-      {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
       <Footer />
     </div>
